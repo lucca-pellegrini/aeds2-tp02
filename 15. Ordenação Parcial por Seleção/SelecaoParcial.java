be6789412f10de572ad2a23c50a7071cadab354d
Lucca Pellegrini <632054@sga.pucminas.br>
2024-10-08 09:12:16 -0300

    15: corrige arquivo de log incorreto

--- 15. Ordenação Parcial por Seleção/SelecaoParcial.java
+++ 15. Ordenação Parcial por Seleção/SelecaoParcial.java
@@ -12,7 +12,7 @@ public class SelecaoParcial
     private static final String DEFAULT_DB = "/tmp/pokemon.csv";
 
     // Contém as estatísticas que o enunciado pede.
-    private static final String LOG = "842986_selecao.txt";
+    private static final String LOG = "842986_selecaoParcial.txt";
     private static final long MATRICULA = 842986;
     private static int numComparacoes = 0;
     private static long tempoExecucao;
